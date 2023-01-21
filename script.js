23f6dedbb37c7c68dc063626cc36b0d220ba1889
a1x02 <Sashurik2002@gmail.com>
2023-01-21 06:57:41 +0300

    feat: реализовать сохранение профиля

--- script.js
+++ script.js
@@ -2,15 +2,39 @@ let profile = document.querySelector('.profile')
 let editButton = profile.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup')
 let closeButton = popup.querySelector('.popup__close')
+let form = popup.querySelector('.popup__form')
+let profileName = profile.querySelector('.profile__name')
+let profileDescription = profile.querySelector('.profile__description')
+let saveButton = form.querySelector('.popup__save-button')
 
 
 function openPopUp() {
+    let formName = form.querySelector('.popup__name')
+    let formDescription = form.querySelector('.popup__description')
+
+    formName.value = profileName.textContent
+    formDescription.value = profileDescription.textContent
+
     popup.classList.add('popup_show', true)
+
+    // console.log(profileName.textContent)
 }
 
 function closePopUp() {
     popup.classList.remove('popup_show')
 }
 
+function saveProfile() {
+    let formName = form.querySelector('.popup__name')
+    let formDescription = form.querySelector('.popup__description')
+
+    profileName.textContent = formName.value
+    profileDescription.textContent = formDescription.value
+
+    formName.value = ''
+    formDescription.value = ''
+}
+
 editButton.addEventListener('click', openPopUp)
 closeButton.addEventListener('click', closePopUp)
+saveButton.addEventListener('click', saveProfile)
