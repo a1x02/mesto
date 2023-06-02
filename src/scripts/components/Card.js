6a9b189f350779ac4714e5ffa7c9960ef24b5434
a1x02 <Sashurik2002@gmail.com>
2023-06-02 14:16:12 +0300

    feat: добавить счетчик лайков в компонент Card

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -1,7 +1,13 @@
 export default class Card {
-    constructor(data, cardTemplate, handleCardClick) {
+    constructor({data, handleCardClick, handleCardLike, handleCardDelete}, cardTemplate, userId) {
         this._cardLink = data.link
         this._cardName = data.name
+        this._cardLikes = data.likes
+        this._cardId = data._id
+        this._ownerId = data.owner._id
+        this._userId = userId
+        this._handleCardLike = handleCardLike
+        this._handleCardDelete = handleCardDelete
         this._cardTemplate = cardTemplate
         this._handleCardClick = handleCardClick
     }
@@ -21,6 +27,8 @@ export default class Card {
         this._cardImage.src = this._cardLink
         this._cardImage.alt = this._cardName
         this._cardElement.querySelector('.element__title').textContent = this._cardName
+        this._likesAmount = this._cardElement.querySelector('.element__like-counter')
+        this._likesAmount.textContent = this._cardLikes.length
 
         this._setEventListeners()
 
@@ -31,12 +39,17 @@ export default class Card {
         this._buttonDelete = this._cardElement.querySelector('.delete-button')
         this._buttonLike = this._cardElement.querySelector('.element__like-button')
 
-        this._buttonDelete.addEventListener('click', () => {
-            this._handleButtonDeleteClick()
-        })
+        if (this._ownerId === this._userId) {
+            this._buttonDelete.addEventListener('click', () => {
+                this._deleteCard(this._cardId)
+            })
+        } else {
+            this._buttonDelete.remove()
+            this._buttonDelete = null
+        }
 
         this._buttonLike.addEventListener('click', () => {
-            this._handleButtonLikeClick()
+            this._handleCardLike(this._cardId)
         })
 
         this._cardImage.addEventListener('click', () => {
@@ -44,12 +57,26 @@ export default class Card {
         })
     }
 
-    _handleButtonLikeClick() {
-        this._buttonLike.classList.toggle('element__like-button_active')
+    _checkIdInLikes() {
+        return this._cardLikes.some(data => data._id === this._userId)
+    }
+
+    setLike(likes) {
+        this._cardLikes = likes
+        this._likesAmount.textContent = likes.length
+        if (this._checkIdInLikes()) {
+            this._buttonLike.classList.add('element__like-button_active')
+        } else {
+            this._buttonLike.classList.remove('element__like-button_active')
+        }
     }
 
     _handleButtonDeleteClick() {
         this._cardElement.remove()
         this._cardElement = null
+    }
+
+    _deleteCard() {
+        this._handleCardDelete(this._cardId)
     }
 }
